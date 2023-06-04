810fb127dbbbe16f4eeb903719384cda3e96e75a
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2023-06-04 08:18:08 -0700

    Update ec2instance.tf

--- 03 - Language Syntax/manifests/ec2instance.tf
+++ 03 - Language Syntax/manifests/ec2instance.tf
@@ -1,6 +1,6 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "ami-0e5b6b6a9f3db6db8"
+  ami = "ami-03f65b8614a860c29"
   instance_type = "t3.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
