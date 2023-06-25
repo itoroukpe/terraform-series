ac81c28fcbcda80f56ed6b6bd92f93d8b07c2838
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2023-06-25 08:44:51 -0700

    Update ec2.tf

--- 05 - Variables and Data Sources/manifests/Terraform-input-variables/01-Basic-variables/ec2.tf
+++ 05 - Variables and Data Sources/manifests/Terraform-input-variables/01-Basic-variables/ec2.tf
@@ -1,7 +1,7 @@
 # Create EC2 Instance
 resource "aws_instance" "my-ec2" {
   ami                    = var.ec2_ami_id
-  instance_type          = "t3.micro"
+  instance_type          = "t2.micro"
   key_name               = "terraform-key"
   count                  = var.ec2_instance_count
   user_data              = <<-EOF
