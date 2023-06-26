8abafe25c075dc2f4fbd05f7187972b34cd4fda8
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2023-06-25 19:56:06 -0700

    Update ec2.tf

--- 05 - Variables and Data Sources/manifests/Terraform-input-variables/06-terraform-tfvars/ec2.tf
+++ 05 - Variables and Data Sources/manifests/Terraform-input-variables/06-terraform-tfvars/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "my-ec2" {
   ami                    = var.ec2_ami_id
   instance_type          = var.ec2_instance_type
-  key_name               = "terraform-key"
+  key_name               = "classkey"
   count                  = var.ec2_instance_count
   user_data              = <<-EOF
     #!/bin/bash
