4afe85ec065bd24f10bada2499b93b5b7d388d5b
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2023-06-25 18:52:26 -0700

    Update variables.tf

--- 05 - Variables and Data Sources/manifests/Terraform-input-variables/08-auto-tfvars/variables.tf
+++ 05 - Variables and Data Sources/manifests/Terraform-input-variables/08-auto-tfvars/variables.tf
@@ -2,13 +2,13 @@
 variable "aws_region" {
   description = "Region in which AWS resources to be created"
   type        = string
-  default     = "us-west-1"
+  default     = "us-west-2"
 }
 
 variable "ec2_ami_id" {
   description = "AMI ID"
   type        = string
-  default     = "ami-0915bcb5fa77e4892" # Amazon2 Linux AMI ID
+  default     = "ami-07dfed28fcf95241c" # Amazon2 Linux AMI ID
 }
 
 variable "ec2_instance_count" {
@@ -20,5 +20,5 @@ variable "ec2_instance_count" {
 variable "ec2_instance_type" {
   description = "EC2 Instance Type"
   type = string
-  default = "t3.micro"
+  default = "t2.micro"
 }
